62bbdd664ccaf20f23f33496dffe38a4ecde8925
diegosebasmassaro <diego.sebasmassaro@gmail.com>
2025-08-14 16:07:26 -0300

    Update tienda y agregue 3 imagenes

---
+++ tienda.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+<header>
+    <div class="fixed-top container-flex">
+<nav class="navbar navbar-expand-lg" style="background-color:#769953">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Crystal dimensions</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="piedras.php">Que Hacemos?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tienda.php">Tienda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacto.php" >Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+</header>
+
+<main style="background-color:#CDEBAE">
+
+
+<div class="container-flex col-4 py-5">
+<div class="card mb-3 col" style="max-width: 540px; background-color:#769953;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="img/Collar.webp" class="img-fluid rounded-start" alt="Foto de accesorios con piedras energeticas">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Collares con con piedras</h5>
+        <p class="card-text">Lorem ipsum dolor sit...</p>
+        <h2 style="color:#FF2E05">Desde $6000</h2>
+        <a href="contacto.php" class="btn btn-danger">Comprar</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width: 540px; background-color:#769953;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="img/Anillos.webp" class="img-fluid rounded-start" alt="Foto de accesorios con piedras energeticas">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Anillos con piedras</h5>
+        <p class="card-text">Lorem ipsum dolor sit...</p>
+        <h2 style="color:#FF2E05">Desde $2500</h2>
+        <a href="contacto.php" class="btn btn-danger">Comprar</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width: 540px; background-color:#769953;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="img/pulsera.webp" class="img-fluid rounded-start" alt="Foto de accesorios con piedras energeticas">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Pulseras con piedras</h5>
+        <p class="card-text">Lorem ipsum dolor sit...</p>
+        <h2 style="color:#FF2E05">Desde $3500</h2>
+        <a href="contacto.php" class="btn btn-danger">Comprar</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+<footer style="background-color:#769953;">
+    <div>
+<p>Todos los derechos reservados - Los Frikis 2025</p>
+</div>
+
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+</html>
